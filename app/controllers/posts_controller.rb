fb09341714450c9a59f97b2b6b024fc664ca4661
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-19 05:28:22 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 	end
 
 	private
-	
+
 	def post_params(*args)
 	  params.require(:post).permit(*args)
 	end
